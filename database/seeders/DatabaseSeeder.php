ccc15139f537aa0f5402cadce214040be4d5dadc
Joao Pedro <joao.fernandes37.pedro@gmail.com>
2026-06-12 17:40:53 -0300

    Ultimo commit da api que foi concluida com sucesso !

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        $this->call([TasksSeeder::class, PeopleSeeder::class]);
+        $this->call([PeopleSeeder::class]);
     }
 }
--- database/seeders/PeopleSeeder.php
+++ database/seeders/PeopleSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\People;
+use App\Models\Tasks;
 use Illuminate\Database\Seeder;
 
 class PeopleSeeder extends Seeder
@@ -10,11 +11,22 @@ class PeopleSeeder extends Seeder
     public function run(): void
     {
         // People::factory()->make()->toArray(); e salvar
-        $person = People::factory()->count(10)->create();
+        $people = People::factory()
+            ->count(10)
+            ->create();
 
-        // $person->tasks()
-        //     ->syncWithoutDetaching(
-        //         [1, 4, 7]
-        //     );
+        $tasks = Tasks::factory()
+            ->count(30)
+            ->create();
+
+        foreach ($people as $person) {
+            $person->tasks()->attach(
+                $tasks
+                    ->random(rand(1, 5))
+                    ->pluck('id')
+                    ->toArray()
+            );
+        }
+        
     }
 }
